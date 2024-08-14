22d3dd963a655d8c71f546b0a824a3185479c908
Yash Saxena <yash.saxena@gmail.com>
2024-08-14 03:09:46 -0700

    halp

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import SubmitPostScreen from "./screens/SubmitPostScreen";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={"/"}>
       <Routes>
         <Route
           index
